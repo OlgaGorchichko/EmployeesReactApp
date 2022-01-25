39351806c38c08ac45aba6520a22858d4c1b9f6b
Olga <olga@gorchichko.com>
2022-01-25 14:42:38 +0300

    added dynamic states for employee-list-item component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,6 +8,13 @@ import './app.css';
 
 
 function App() {
+
+    const data = [
+        {name: 'John C.', salary: 800, increase: false, id: 1},
+        {name: 'Alex M.', salary: 3000, increase: true, id: 2},
+        {name: 'Carl W.', salary: 5000, increase: false, id: 3}
+    ];
+
     return (
         <div className="app">
             <AppInfo/>
@@ -15,7 +22,7 @@ function App() {
             <div className="search-panel">
                 <SearchPanel/>
                 <AppFilter/>
-                <EmployeesList/>
+                <EmployeesList data={data}/>
                 <EmployeesAddForm/>
             </div>
         </div>
